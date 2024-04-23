ecd0b8daec0a8a9b8fc395de143755492cbe86d8
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2024-04-23 14:10:07 +0545

    project initialization

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "#003153",
+          200: "rgba(100, 149, 237, 0.4)",
+          300: "rgba(137, 207, 240, 0.8)",
+        },
+        yellow: "#faa80a",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
