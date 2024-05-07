3a30579db60f601602d256088ffc1a38d17150c2
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2024-05-07 22:18:03 +0545

    university section wip

---
+++ data.js
@@ -0,0 +1,18 @@
+const univ = [
+  {
+    id: 1,
+    name: "Pokhara University",
+  },
+  {
+    id: 2,
+    name: "Tribhuvan University",
+  },
+  {
+    id: 3,
+    name: "Kathmandu University",
+  },
+  {
+    id: 4,
+    name: "Khowpa University",
+  },
+];
