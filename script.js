8c93f18086f9c12892ec5d081e55859c49651ddd
Raj Kiran Chaudhary <raj.chaudhary@ebpearls.com>
2024-05-23 21:50:11 +0545

    courses section

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ const univs = [
 const subjects = [
   {
     id: 1,
-    sub: "Engineering Maths",
+    sub: "Engineering Mathematics I",
     sem: "I",
     desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam nisi aut excepturi iure tenetur at",
     credit: 3,
@@ -73,10 +73,12 @@ const subjects = [
 
 // university
 const university = document.querySelector(".university");
+const course = document.querySelector(".course-box");
 
+// University list here
 const getUniversityList = () => {
   let universityList = univs.map((univ) => {
-    return `<div class="cursor-pointer">
+    return `<div class="cursor-pointer" key="${univ.id}">
       <h5 class="font-semibold">${univ.name}</h5>
         <p class="font-light text-sm">${univ.numberOfPapers} papers</p>
       </div>`;
@@ -86,6 +88,43 @@ const getUniversityList = () => {
 
 window.addEventListener("DOMContentLoaded", () => {
   getUniversityList();
+});
+
+// Course list here
+const getCourseList = () => {
+  let courseList = subjects.map((subject) => {
+    return `
+    <div
+    key=${subject.id}
+    class="p-4 cursor-pointer rounded-xl bg-white border-2 border-primary-100 group transition duration-200 ease-linear hover:bg-primary-100"
+  >
+    <div class="flex justify-between">
+      <p class="font-medium text-xl group-hover:text-white">Semestar</p>
+      <p class="font-medium text-xl group-hover:text-white">${subject.sem}</p>
+    </div>
+    <h5 class="font-medium mt-4 mb-2 text-md group-hover:text-white">
+      ${subject.sub}
+    </h5>
+    <p class="font-normal text-sm mb-4 group-hover:text-white">
+      ${subject.desc}
+    </p>
+    <div
+      class="px-2 py-1 text-primary-100 border-2 border-primary-100 font-normal text-sm rounded-3xl max-w-32 flex group-hover:border-white"
+    >
+      <div class="flex justify-between w-full">
+        <span class="group-hover:text-white">Credit</span>
+        <span class="group-hover:text-white">${subject.credit} hrs</span>
+      </div>
+    </div>
+  </div>
+    `;
+  });
+
+  course.innerHTML = courseList.join("");
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  getCourseList();
 });
 
 // footer year
